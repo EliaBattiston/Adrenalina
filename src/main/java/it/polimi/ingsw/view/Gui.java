8a9f6a8e6f5af90a122535a9e5ded0454b72b8b4
AndreaAspesiPoli <andrea.aspesi@mail.polimi.it>
2019-05-01 20:03:35 +0200

    Initial look to gui, added maps images

---
+++ src/main/java/it/polimi/ingsw/view/Gui.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.view;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+public class Gui extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        double width, height;
+        width = 800;
+        height = 600;
+
+        primaryStage.setTitle("Adrenalina");
+/*
+        StackPane layout = new StackPane();
+
+        BackgroundImage myBI= new BackgroundImage(new Image("file:images/map1.png",width,height,true,true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+//then you set to your node
+        layout.setBackground(new Background(myBI));
+
+        Scene sc = new Scene(layout, width, height);
+
+        // primaryStage.setScene(s);
+        //primaryStage.show();
+*/
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        primaryStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( width, height);
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        //gc.setFill( Color.RED );
+        //gc.setStroke( Color.BLACK );
+        //gc.setLineWidth(2);
+        //Font theFont = Font.font( "Times New Roman", FontWeight.BOLD, 48 );
+        //gc.setFont( theFont );
+        //gc.fillText( "Hello, World!", 60, 50 );
+        //gc.strokeText( "Hello, World!", 60, 50 );
+
+        Image earth = new Image( "file:images/map1.png" );
+        gc.drawImage( earth, 0, 0, width,height );
+
+        primaryStage.show();
+    }
+}
