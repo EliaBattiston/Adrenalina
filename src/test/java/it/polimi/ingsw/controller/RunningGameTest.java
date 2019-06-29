bd47e95e98394a7bc477a962b59d9cabb3fc4243
AndreaAspesiPoli <andrea.aspesi@mail.polimi.it>
2019-06-29 11:49:47 +0200

    Divided the AIGame tests

--- src/test/java/it/polimi/ingsw/controller/RunningGameTest.java
+++ src/test/java/it/polimi/ingsw/controller/RunningGameTest.java
@@ -10,23 +10,57 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class RunningGameTest {
+
+    /**
+     * Run a game with 3 AI players and tests it ends without generating issues
+     */
     @Test
-    public void multipleGames(){
-        for(int i = 0; i<5; i++)
-            new Thread(this::checkGameWithAI).run();
+    public void testThreePlayers(){
+        new Thread(()->
+                checkGameWithAI(3)
+        ).run();
+        new Thread(()->
+                checkGameWithAI(3)
+        ).run();
+    }
+
+    /**
+     * Run a game with 4 AI players and tests it ends without generating issues
+     */
+    @Test
+    public void testFourPlayers(){
+        new Thread(()->
+                checkGameWithAI(4)
+        ).run();
+        new Thread(()->
+                checkGameWithAI(4)
+        ).run();
+    }
+
+    /**
+     * Run a game with 5 AI players and tests it ends without generating issues
+     */
+    @Test
+    public void testFivePlayers(){
+        new Thread(()->
+                checkGameWithAI(5)
+        ).run();
+        new Thread(()->
+                checkGameWithAI(5)
+        ).run();
     }
 
     /**
      * Run a server and some AI clients, wait until the end of the match. Check the game ends with no problems
      */
-    private void checkGameWithAI(){
-        try
-        {
-            Match m = new Match(8);
+    private void checkGameWithAI(int numOfPlayers){
+        try {
+            int skullsNum = ((int)(Math.random()*4))+5;
+            Match m = new Match(skullsNum);
 
             Player simulated;
-            for(int i=0; i<5; i++) {
-                simulated = new Player(Integer.toString(i), "Testing is hard work, but someone has to do it!", Fighter.values()[i]);
+            for(int i=0; i<numOfPlayers; i++) {
+                simulated = new Player(Integer.toString(i +1), "Testing is hard work, but someone has to do it!", Fighter.values()[i]);
                 simulated.setConn(new AIConnection());
                 m.getGame().addPlayer(simulated);
             }
@@ -40,13 +74,10 @@ public class RunningGameTest {
                 fail();
             }
 
+            System.out.println("\n\n\u001B[32mPartita completata con successo\u001B[0m\n\n\n\n");
             assertTrue(true);
-        }
-        catch(FileNotFoundException e)
-        {
+        } catch(FileNotFoundException e) {
             fail();
         }
-
     }
-
 }
